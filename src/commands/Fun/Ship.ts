ba95988a65042d701ad4a32baf3a686ef9efad38
Saram Khan <97223403+saramkhani@users.noreply.github.com>
2022-05-30 01:39:12 +0500

    Update Ship.ts

--- src/commands/Fun/Ship.ts
+++ src/commands/Fun/Ship.ts
@@ -2,64 +2,123 @@ import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ship',
-            description: `Ship people with you or someone`,
+            command: 'react',
+            description: `Let's React`,
+            aliases: [
+                'r',
+                'cry',
+                'kiss',
+                'bully',
+                'hug',
+                'lick',
+                'cuddle',
+                'pat',
+                'smug',
+                'highfive',
+                'bonk',
+                'yeet',
+                'blush',
+                'wave',
+                'smile',
+                'handhold',
+                'nom',
+                'bite',
+                'glomp',
+                'kill',
+                'slap',
+                'cringe',
+                'kick',
+                'wink',
+                'happy',
+                'poke',
+                'dance'
+            ],
             category: 'fun',
-            usage: `${client.config.prefix}ship [tag/quote user]`,
+            usage: `${client.config.prefix}(reaction) [tag/quote users]\nExample: ${client.config.prefix}pat`,
             baseXp: 200
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        const percentage = Math.floor(Math.random() * 100)
-        let sentence
-        if (percentage < 25) {
-            sentence = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\tThere's still time to reconsider your choices`
-        } else if (percentage < 50) {
-            sentence = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t Good enough, I guess! 💫`
-        } else if (percentage < 75) {
-            sentence = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t\tStay together and you'll find a way ⭐️`
-        } else if (percentage < 90) {
-            sentence = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tAmazing! You two will be a good couple 💖 `
-        } else {
-            sentence = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tYou two are fated to be together 💙`
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        // make key value pairs of reactions like this: { 'bully': 'bullied', 'cuddle': 'cuddled', ... }
+        // Access raw text of message
+        const action = M.content?.split(' ')[0].slice(1).toLowerCase() || ''
+        let flag = true
+        if (!(action === 'r' || action === 'react')) {
+            flag = false
         }
-
-        if (M.quoted?.sender && !M.mentioned.includes(M.quoted.sender)) M.mentioned.push(M.quoted.sender)
-        while (M.mentioned.length < 2) M.mentioned.push(M.sender.jid)
-        const user1 = M.mentioned[0]
-        const user2 = M.mentioned[1]
-        const data = JSON.parse((this.client.assets.get('ship') as Buffer)?.toString()) as unknown as {
-            shipJson: {
-                id: number
-                shipPercent: string
-                gifLink: string
-            }[]
-        }
-
-        const ship = data.shipJson.filter((ship) => {
-            const shipPercent = parseInt(ship.shipPercent)
-            return Math.abs(shipPercent - percentage) <= 10
+        const Reactions = {
+            cry: ['Cried with', 'is Crying by'],
+            kiss: ['Kissed'],
+            bully: ['Bullied'],
+            hug: ['Hugged'],
+            lick: ['Licked'],
+            cuddle: ['Cuddled with'],
+            pat: ['Patted'],
+            smug: ['Smugged at', 'is Smugging by'],
+            highfive: ['High-fived'],
+            bonk: ['Bonked'],
+            yeet: ['Yeeted'],
+            blush: ['Blushed at', 'is Blushing by'],
+            wave: ['Waved at'],
+            smile: ['Smiled at', 'is Smiling by'],
+            handhold: ['is Holding Hands with'],
+            nom: ['is Eating with', 'is Eating by'],
+            bite: ['Bit'],
+            glomp: ['Glomped'],
+            kill: ['Killed'],
+            slap: ['Slapped'],
+            cringe: ['Cringed at'],
+            kick: ['Kicked'],
+            wink: ['Winked at'],
+            happy: ['is Happy with', 'is Happy by'],
+            poke: ['Poked'],
+            dance: ['is Dancing with', 'is Dancing by']
+        } as unknown as { [key: string]: string[] }
+        // take the first argument and make it lowercase
+        const term = flag ? joined.split(' ')[0].toLowerCase() : action
+        let text = ''
+        // map over reactions and add index + reaction to text
+        Object.keys(Reactions).map((reaction) => {
+            text += `📍${reaction.charAt(0).toUpperCase() + reaction.slice(1)}\n`
+            // if index is multiple of 3, add a new line, else give 10 - length of the reaction spaces
+            // index % 3 === 2 ? (text += '\n') : (text += ' '.repeat(10 - reaction.length))
         })
-        // choose a random gif from the array
-        const gifLink = ship[Math.floor(Math.random() * ship.length)].gifLink
-        let caption = `\t❣️ *Matchmaking...* ❣️ \n`
-        caption += `\t\t---------------------------------\n`
-        caption += `@${user1.split('@')[0]}  x  @${user2.split('@')[0]}\n`
-        caption += `\t\t---------------------------------\n`
-        caption += `${sentence}`
-
-        return void M.reply(
-            await this.client.util.GIFBufferToVideoBuffer(await this.client.getBuffer(gifLink)),
+        text += `🎀 *Usage:* ${this.client.config.prefix}(reaction) [tag/quote users]\nExample: ${this.client.config.prefix}pat`
+        if (flag) {
+            if (!term) return void M.reply(`🪧 *OPTIONS:*\n${text}`)
+            if (!Object.keys(Reactions).includes(term))
+                return void M.reply(
+                    `✖  No Reaction Found 🧧\nUse ${this.client.config.prefix}r to see all available reactions`
+                )
+        }
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length) M.mentioned.push(M.sender.jid)
+        // remove duplicate mentions
+        M.mentioned = [...new Set(M.mentioned)]
+        let grammar: string
+        M.mentioned[0] === M.sender.jid
+            ? (grammar = Reactions[`${term}`].pop() || Reactions[`${term}`][0])
+            : (grammar = Reactions[`${term}`][0])
+        M.reply(
+            await this.client.util.GIFBufferToVideoBuffer(
+                await this.client.getBuffer(
+                    (
+                        await this.client.fetch<{ url: string }>(`https://api.waifu.pics/sfw/${term}`)
+                    ).url
+                )
+            ),
             MessageType.video,
             Mimetype.gif,
-            [user1, user2],
-            caption
+            [M.sender.jid, ...M.mentioned],
+            `*@${M.sender.jid.split('@')[0]} ${grammar} ${M.mentioned
+                .map((user) => (user === M.sender.jid ? 'Themselves' : `@${user.split('@')[0]}`))
+                .join(', ')}*`
         )
     }
 }
